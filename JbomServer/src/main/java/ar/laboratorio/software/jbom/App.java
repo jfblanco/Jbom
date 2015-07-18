492dda6fa355b17990a18f246937839b47d2f38c
Francisco Blanco <blanco.jose.francisco@gmail.com>
2015-07-18 12:42:44 -0300

    primer commit

---
+++ JbomServer/src/main/java/ar/laboratorio/software/jbom/App.java
@@ -0,0 +1,13 @@
+package ar.laboratorio.software.jbom;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
